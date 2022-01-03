48c42e311dc924f69be404880bce554ec9b3a1a0
salam <es_belhadj@esi.dz>
2022-01-03 21:22:24 +0100

    Add the remove feature

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const myStorage = window.localStorage;
 
-const books = (myStorage.getItem('books') !== null) ? JSON.parse(myStorage.getItem('books')) : [];
+let books = (myStorage.getItem('books') !== null) ? JSON.parse(myStorage.getItem('books')) : [];
 
 books.forEach((data) => {
   document.querySelector('#listbooks').innerHTML += `
             <li>
                 <p>${data.title}</p>
                 <p>${data.author}</p>
-                <button>remove</button>
+                <button class='removeBook' data-book-name="${data.title}" >remove</button>
                 <hr>
             </li>
         `;
@@ -26,12 +26,19 @@ const addBook = (bookTitle, bookAuthor) => {
             <li>
                 <p>${bookTitle.value}</p>
                 <p>${bookAuthor.value}</p>
-                <button type="button">remove</button>
+                <button type="button" data-book-name="${book.title}" onclick='removeBook(this)' >remove</button>
                 <hr>
             </li>
         `;
     myStorage.setItem('books', JSON.stringify(books));
   }
+};
+
+const removeBook = (button) => {
+  const result = books.filter((word) => word.title !== button.getAttribute('data-book-name'));
+  books = result;
+  button.parentElement.remove();
+  myStorage.setItem('books', JSON.stringify(books));
 };
 
 document.querySelector('#addbookform').addEventListener('submit', (e) => {
@@ -41,4 +48,8 @@ document.querySelector('#addbookform').addEventListener('submit', (e) => {
   addBook(title, author);
   title.value = '';
   author.value = '';
+});
+
+document.querySelectorAll('.removeBook').forEach((deleteBook) => {
+  deleteBook.addEventListener('click', () => removeBook(deleteBook));
 });
