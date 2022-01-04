4bee4b1efeafdf4100a54196fe4a2a5deb05f77b
salam <es_belhadj@esi.dz>
2022-01-04 15:21:42 +0100

    use JavaScript classes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class Books {
     const result = this.db().filter((word) => word.title !== button.getAttribute('data-book-name'));
     button.parentElement.remove();
     window.localStorage.setItem('books', JSON.stringify(result));
+    this.showAlert('Book deleted ', 'success');
   }
 
   static load = () => {
@@ -22,39 +23,52 @@ class Books {
             `;
     });
   }
+
+   static addBook = (book) => {
+     const result = this.db().filter((word) => word.title === book.title);
+     if (result.length === 0) {
+       const data = this.db();
+       data.push(book);
+       window.localStorage.setItem('books', JSON.stringify(data));
+       document.querySelector('#listbooks').innerHTML += `
+        <li>
+            <p><q>${book.title}</q> by ${book.author}</p>
+            <button type="button" data-book-name="${book.title}" onclick='removeBook(this)' >remove</button>
+        </li>
+        `;
+       this.showAlert('Book added ', 'success');
+     } else {
+       this.showAlert('Book already exists ', 'danger');
+     }
+   }
+
+   static showAlert(message, className) {
+     const div = document.createElement('div');
+     div.className = `alert ${className}`;
+     div.appendChild(document.createTextNode(message));
+     const container = document.querySelector('#addbookform');
+     const formtitle = document.querySelector('#formtitle');
+     container.insertBefore(div, formtitle);
+     setTimeout(() => document.querySelector('.alert').remove(), 3000);
+   }
 }
 
 (() => {
   Books.load();
 })();
 
-const addBook = (bookTitle, bookAuthor) => {
-  const book = {
-    title: '',
-    author: '',
-  };
-  if (bookTitle.value !== '' && bookAuthor.value !== '') {
-    book.title = bookTitle.value;
-    book.author = bookAuthor.value;
-    books.push(book);
-    document.querySelector('#listbooks').innerHTML += `
-            <li>
-                <p><q>${bookTitle.value}</q> by ${bookAuthor.value}</p>
-                <button type="button" data-book-name="${book.title}" onclick='removeBook(this)' >remove</button>
-            </li>
-        `;
-    myStorage.setItem('books', JSON.stringify(books));
-  }
-};
-
-
 document.querySelector('#addbookform').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.querySelector('#title');
   const author = document.querySelector('#author');
-  addBook(title, author);
-  title.value = '';
-  author.value = '';
+  if (title.value !== '' && author.value !== '') {
+    const BookObj = new Books(title.value, author.value);
+    Books.addBook(BookObj);
+    title.value = '';
+    author.value = '';
+  } else {
+    Books.showAlert('Please fill all the fields', 'danger');
+  }
 });
 
 document.querySelectorAll('.removeBook').forEach((deleteBook) => {
